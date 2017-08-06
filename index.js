5c92d2ecb2bdb9803c308ebcd8d984bdad5ddc12
kudou-reira <h.i.y.e.o.XD@gmail.com>
2017-08-07 03:07:10 +0900

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 //server-side: common modules use 'require'
 
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./config/keys');
+
 const app = express();
+
+// client ID: 317830939684-7lk7gihk3aifpv2k5bspove2qh9ikh03.apps.googleusercontent.com
+// client secret: ZOVFND7Ag62hzfSNucg9PFdr
+
+//passport, make use of this Google oauth
+passport.use(new GoogleStrategy({
+		clientID: keys.googleClientID,
+		clientSecret: keys.googleClientSecret,
+		callbackURL: '/auth/google/callback'
+	}, (accessToken) => {
+		console.log(accessToken);
+	})
+);
 
 console.log('Server running');
 
 //watch for incoming requests with this method
-app.get('/', (req, res) => {
-	res.send({hi: 'there'});
-});
+//scope asks google to give back profile and email information
+app.get('/auth/google', passport.authenticate('google', {
+		scope: ['profile', 'email']
+	}) 
+);
+
+//'code' gets sent in url, google strategy will see the code and try to handle
+// callback sends back an access token, this means it's successful
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
